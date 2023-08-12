b5d7c8050b8d8b927ab4dbabfe4fc5a055815673
Shubham <shubhamkumar101100@gmail.com>
2023-08-13 01:22:10 +0530

    Fix: Url fixing

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import  home, post, edit_post, search, profile, upload, contact, privacy_policy
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('upload/', upload, name='upload'),
-    path('post/<str:post_url>', post, name='post'),
-    path('post/<str:post_url>/edit/', edit_post, name='edit_post'),
+    path('posts/', home, name='posts'),
+    re_path('^posts/(?P<post_url>[\w-]+)/$', post, name='post'),
+    path('posts/<str:post_url>/edit/', edit_post, name='edit_post'),
     path('search/', search, name='search'),
     path('accounts/profile/', profile, name='profile'),
 
